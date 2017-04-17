c2f31c3798234178802455f7e77fc605bd6956f1
gerss <gersson.fuenzalida@alumnos.usm.cl>
2017-04-17 20:09:39 -0300

    Name of the steps

--- add.cu
+++ add.cu
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+//KERNEL
 __global__ void Add(int *a, int *b)
 {
 a[0] += b[0];
@@ -9,17 +10,23 @@ a[0] += b[0];
 
 int main()
 {
+
+//ALLOCATE AND INITIALIZA DATA ON CPU
 int a = 5, b = 9;
 int *d_a, *d_b;
 
+//ALLOCATE DATA ON GPU
 cudaMalloc(&d_a, sizeof(int));
 cudaMalloc(&d_b, sizeof(int));
 
+//TRANSFER DATA FROM CPU TO GPU
 cudaMemcpy(d_a, &a, sizeof(int), cudaMemcpyHostToDevice);
 cudaMemcpy(d_b, &b, sizeof(int), cudaMemcpyHostToDevice);
 
+//RUN KERNEL
 Add<<<1,1>>>(d_a, d_b);
 
+//TRANSFER DATA FROM GPU TO CPU
 cudaMemcpy(&a, d_a, sizeof(int), cudaMemcpyDeviceToHost);
 
 cout<<"The answer is "<<a<<endl;
