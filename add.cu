e11cd6f0094661a7bc096483ee3a20ca99b1392c
gerss <gersson.fuenzalida@alumnos.usm.cl>
2017-04-17 20:01:29 -0300

    the first version

---
+++ add.cu
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+__global__ void Add(int *a, int *b)
+{
+a[0] += b[0];
+}
+
+int main()
+{
+int a = 5, b = 9;
+int *d_a, *d_b;
+
+cudaMalloc(&d_a, sizeof(int));
+cudaMalloc(&d_b, sizeof(int));
+
+cudaMemcpy(d_a, &a, sizeof(int), cudaMemcpyHostToDevice);
+cudaMemcpy(d_b, &b, sizeof(int), cudaMemcpyHostToDevice);
+
+Add<<<1,1>>>(d_a, d_b);
+
+cudaMemcpy(&a, d_a, sizeof(int), cudaMemcpyDeviceToHost);
+
+cout<<"The answer is "<<a<<endl;
+
+cudaFree(d_a);
+cudaFree(d_b);
+
+return 0;
+}
