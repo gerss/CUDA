fe975944721bbe95d54bb6cab83de250e7a2b75d
gerss <gersson.fuenzalida@alumnos.usm.cl>
2017-04-17 21:27:35 -0300

    Add time measurement

--- add.cu
+++ add.cu
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <sys/time.h>
 
 using namespace std;
+
+double get_time()
+{  struct timeval tim;
+   gettimeofday(&tim, NULL);
+   return (double) tim.tv_sec+(tim.tv_usec/1000000.0);
+}
 
 //KERNEL
 __global__ void Add(int *a, int *b)
@@ -23,15 +30,20 @@ cudaMalloc(&d_b, sizeof(int));
 cudaMemcpy(d_a, &a, sizeof(int), cudaMemcpyHostToDevice);
 cudaMemcpy(d_b, &b, sizeof(int), cudaMemcpyHostToDevice);
 
+double start = get_time(); //Initial time
 //RUN KERNEL
 Add<<<1,1>>>(d_a, d_b);
+double finish = get_time(); //Final time
+
+double diff = finish - start;
+cout<<"time ="<<diff<<endl;
 
 //TRANSFER DATA FROM GPU TO CPU
 cudaMemcpy(&a, d_a, sizeof(int), cudaMemcpyDeviceToHost);
 
-cout<<"The answer is "<<a<<endl;
+cout<<"Total time is : "<<a<<endl;
 
-//Free memory
+//FREE MEMORY
 cudaFree(d_a);
 cudaFree(d_b);
 
